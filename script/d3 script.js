bf10edd3a39c9be57a8ee7b7b748273b167fde55
KynanPell <5849683@student.swin.edu.au>
2024-10-04 11:49:01 +1000

    Update d3 script.js

--- script/d3 script.js
+++ script/d3 script.js
@@ -66,8 +66,8 @@ function init() {
             var xDate = xScale.invert(xPosition);
 
             // Find closest data point for Brazil
-            var brazilData = dataset.filter(function(d) { return d.country == country; });
-            var closestPoint = brazilData.reduce(function(prev, curr) {
+            var countryData = dataset.filter(function(d) { return d.country == country; });
+            var closestPoint = d.reduce(function(prev, curr) {
               return (Math.abs(curr.year - xDate) < Math.abs(prev.year - xDate) ? curr : prev);
             });
 
