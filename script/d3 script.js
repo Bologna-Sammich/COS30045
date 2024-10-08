243bf42feb9ec5d13706f806ed603e49604ae965
KynanPell <5849683@student.swin.edu.au>
2024-10-08 14:36:57 +1100

    Update d3 script.js

--- script/d3 script.js
+++ script/d3 script.js
@@ -80,7 +80,7 @@ function init() {
 
         var countries = Array.from(dataset.map(d => d.country))
 
-        var colorScale = d3.scaleOrdinal(d3.schemeCategory40);
+        var colorScale = d3.scaleOrdinal(d3.schemeCategory20);
 
         countries.forEach(function(country, i) {
           //Draw actual line
