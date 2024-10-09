07a44223e0846b1bd9e1a344a6286e434449aaf1
Bologna-Sammich <sam.wright.urquhart@gmail.com>
2024-10-09 11:46:44 +1100

    Update public-primary-voluntary_bar.js

--- script/public-primary-voluntary_bar.js
+++ script/public-primary-voluntary_bar.js
@@ -224,6 +224,14 @@ d3.selectAll("#check").on("change", function() {
 		
 	} else {
 		svg.select("#"+selected).remove();
+		
+		svg
+		.append('g')
+		.attr("transform", `translate(0,${height})`)
+		.call(d3.axisBottom(xScale).tickSize(0).tickPadding(8))
+		.selectAll("text")
+		.attr("transform", "rotate(45)")
+		.style("text-anchor", "start");
 	}
 });
 });
